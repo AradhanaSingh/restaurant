529bcaecb4d5abf2728c3cf838d861341aa61fff
Aradhana Singh <asingh17@ncsu.edu>
2015-08-30 14:34:44 -0400

    added feature to rename a restaurant

--- webserver.py
+++ webserver.py
@@ -65,6 +65,29 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.wfile.write(output)
                 return
 
+            if self.path.endswith("/edit"):
+                restaurantIDPath = self.path.split("/")[2]
+                myRestaurantQuery = session.query(Restaurant).filter_by(id = restaurantIDPath).one()
+                if myRestaurantQuery != []:
+                    self.send_response(200)
+                    self.send_header('Content-type','text/html')
+                    self.end_headers()
+                    output = ""
+                    output += "<html><body>"
+                    output += "<h1>"
+                    output += myRestaurantQuery.name
+                    output += "</h1>"
+
+                    output += "<form method='POST' enctype='multipart/form-data' action = '/restaurants/%s/edit'>" %restaurantIDPath
+                    output += "<input name = 'editRestaurantName' type='text' placeholder = '%s'>" %myRestaurantQuery.name
+                    output += "<input type='submit' value='Rename'>"
+
+                    output += "</form>"
+
+                    output += "</html></body>"
+                    self.wfile.write(output)
+                    return
+
             if self.path.endswith("/restaurants"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
@@ -78,7 +101,7 @@ class webserverHandler(BaseHTTPRequestHandler):
                 for restaurant in restaurants:
                     output += restaurant.name
                     output += "<br>"
-                    output += "<a href='#'>Edit</a>"
+                    output += "<a href='/restaurants/%s/edit'>Edit</a>" %restaurant.id
                     output += "<br>"
                     output += "<a href='#'>Delete</a>"
                     output += "<br>"
@@ -127,10 +150,28 @@ class webserverHandler(BaseHTTPRequestHandler):
 
                     self.send_response(301)
                     self.send_header('Content-type','text/html')
-                    # shows /restaurants page
+                    # redirect to  /restaurants page
                     self.send_header('Location','/restaurants')
                     self.end_headers()
 
+            if self.path.endswith("/edit"):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get("editRestaurantName")
+                    restaurantIDPath = self.path.split("/")[2]
+                    myRestaurantQuery = session.query(Restaurant).filter_by( id = restaurantIDPath).one()
+                    if myRestaurantQuery != [] :
+                        myRestaurantQuery.name = messagecontent[0]
+                        session.add(myRestaurantQuery)
+                        session.commit()
+
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text-html')
+                    # added a redirect to get back to restaurants page
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
+                return
         except Exception as e:
             print e
 

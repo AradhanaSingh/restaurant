a31fb1bdf904a70d6ad9573b74eb61f5ca1b184f
Aradhana Singh <asingh17@ncsu.edu>
2015-08-30 13:23:15 -0400

    implemented feature to add new restaurant

--- webserver.py
+++ webserver.py
@@ -12,57 +12,68 @@ import cgi
 from databasesetup import Base, Restaurant, MenuItem
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 # Create session and connect to DB
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
-DBSession = sessionmaker(bind = engine)
+DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-
 class webserverHandler(BaseHTTPRequestHandler):
-	# overrides do_GET method
-	def do_GET(self):
-		try:
-			if self.path.endswith("/hello"):
-				self.send_response(200)
-				self.send_header('Content-type', 'text/html')
-				# sends a blank line and marks the end of the header
-				self.end_headers()
+    # overrides do_GET method
+    def do_GET(self):
+        try:
+            if self.path.endswith("/hello"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                # sends a blank line and marks the end of the header
+                self.end_headers()
 
-				output = ""
+                output = ""
                 # added html form
-				output += "<html><body>Hello<form method='POST' enctype='multipart/form-data' action='/hello'> <h2>What would you like me to say?</h2><input name='message' type='text'><input type='submit' value='Submit'></form> " 
-				output += "</body></html>"     
-				self.wfile.write(output)
-				return
+                output += "<html><body>Hello<form method='POST' enctype='multipart/form-data' action='/hello'> <h2>What would you like me to say?</h2><input name='message' type='text'><input type='submit' value='Submit'></form> "
+                output += "</body></html>"
+                self.wfile.write(output)
+                return
 
-			if self.path.endswith("/hola"):
-				self.send_response(200)
-				self.send_header('Content-type', 'text/html')
-				# sends a blank line and marks the end of the header
-				self.end_headers()
+            if self.path.endswith("/hola"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                # sends a blank line and marks the end of the header
+                self.end_headers()
 
-				output = ""
+                output = ""
                 # added html form
-				output += "<html><body>&#161Hola <a href='/hello'>Back to Hello</a><form method='POST' enctype='multipart/form-data' action='/hello'> <h2>What would you like me to say?</h2><input name='message' type='text'><input type='submit' value='Submit'></form> "
-            
-				# wfile method sends the response back to the client 
-				self.wfile.write(output)
-				print output
-				return
-            
-		except IOError:
-			self.send_error(404, "File not found %s" % self.path)
-            
-			if self.path.endswith("/restaurants"):
-				self.send_response(200)
-				self.send_header('Content-type', 'text/html')
-				# sends a blank line and marks the end of the header
-				self.end_headers()
+                output += "<html><body>&#161Hola <a href='/hello'>Back to Hello</a><form method='POST' enctype='multipart/form-data' action='/hello'> <h2>What would you like me to say?</h2><input name='message' type='text'><input type='submit' value='Submit'></form> "
 
-				output = ""
+                # wfile method sends the response back to the client
+                self.wfile.write(output)
+                print output
+                return
+
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text-html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                output += "<h1> Make a New Restaurant </h1>"
+                output += "<form method = 'POST' enctype = 'multipart/form-data' action = '/restaurants/new'>"
+                output += "<input name = 'newRestaurantName' type = 'text' placeholder = 'New Restaurant Name' >"
+                output += "<input type = 'submit' value = 'Create' >"
+                output += "</form>"
+                output += "</html></body>"
+                self.wfile.write(output)
+                return
+
+            if self.path.endswith("/restaurants"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                # sends a blank line and marks the end of the header
+                self.end_headers()
+                output = ""
                 output = "<html><body>"
+                # restaurant id
+                output += "<a href='/restaurants/new'>Make a new restaurant</a><br>"
                 restaurants = session.query(Restaurant).all()
                 for restaurant in restaurants:
                     output += restaurant.name
@@ -76,23 +87,24 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.wfile.write(output)
                 print output
                 return
-            
-            
-        def do_POST(self):
-            try:
+        except IOError:
+            self.send_error(404, "File not found %s" % self.path)
+
+    def do_POST(self):
+        try:
+            if self.path.endswith("/hello") or self.path.endswith("/halo"):
                 self.send_response(301)
                 self.end_headers()
-                
-                # cgi.parse_header method parses html form header into a main value and dictionary of parameters  
+                # cgi.parse_header method parses html form header into a main value and dictionary of parameters
                 ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-                
+
                 # to check if form data is being received
                 if ctype == 'multipart/form-data':
                     # fields would store all the data in the form
                     fields = cgi.parse_multipart(self.rfile, pdict)
                     # get the value/values for a specific field named "message", will call the field "message when HTML form will be created"
                     messagecontent = fields.get('message')
-                    
+
                     # we have parsed the data given by the user
                     output = ""
                     output += "<html><body>"
@@ -100,24 +112,44 @@ class webserverHandler(BaseHTTPRequestHandler):
                     output += "<h1> %s </h1>" % messagecontent[0]
                     output += "</html></body>"
                     self.wfile.write(output)
-            except Exception as e:
-                print e
 
-			
+            if self.path.endswith("/restaurants/new"):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    # parsing to get name of the new restaurant
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+
+                    # Create a new Restaurant class
+                    newRestaurant = Restaurant(name = messagecontent[0])
+                    session.add(newRestaurant)
+                    session.commit()
+
+                    self.send_response(301)
+                    self.send_header('Content-type','text/html')
+                    # shows /restaurants page
+                    self.send_header('Location','/restaurants')
+                    self.end_headers()
+
+        except Exception as e:
+            print e
+
+
 def main():
-	try:
-		port = 8080
-		hostname = ''
-		# starts the web server
-		# handler is class webServerHandler
-		server = HTTPServer((hostname, port), webserverHandler)
-		print "Web server running on port %s " % port
-		# webserver will run until ^C is hit or when the user exits the application
-		server.serve_forever()
+    server = ""
+    try:
+        port = 8080
+        hostname = ''
+        # starts the web server
+        #  handler is class webServerHandler
+        server = HTTPServer((hostname, port), webserverHandler)
+        print "Web server running on port %s " % port
+        # webserver will run until ^C is hit or when the user exits the application
+        server.serve_forever()
+    except Exception as e:
+        server.socket.close()
+        print "^C entered, stopping the web server"
 
-	except Exception as e:
-		server.socket.close()
-		print "^C entered, stopping the web server"
 
 if __name__ == "__main__":
-	main()
+    main()
